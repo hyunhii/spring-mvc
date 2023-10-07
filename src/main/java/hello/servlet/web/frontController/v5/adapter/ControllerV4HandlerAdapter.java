0a8aebbe4c8d1aabb62b46dfafd003d280e8d085
hyunhii <be_hyunny@naver.com>
2023-10-07 09:49:22 +0900

    feat: 4.MVC 프레임워크 만들기 - 유연한 컨트롤러2 - v5

---
+++ src/main/java/hello/servlet/web/frontController/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,39 @@
+package hello.servlet.web.frontController.v5.adapter;
+
+import hello.servlet.web.frontController.ModelView;
+import hello.servlet.web.frontController.v4.ControllerV4;
+import hello.servlet.web.frontController.v5.MyHandlerAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.HashMap;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean support(Object handler) {
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) {
+        ControllerV4 controller = (ControllerV4) handler;
+
+        HashMap<String, String> paramMap = createParamMap(request);
+        HashMap<String, Object> model = new HashMap<>();
+
+        String viewName = controller.process(paramMap, model);
+
+        ModelView modelView = new ModelView(viewName);
+        modelView.setModel(model);
+
+        return modelView;
+    }
+
+    private HashMap<String, String> createParamMap(HttpServletRequest request) {
+        HashMap<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
