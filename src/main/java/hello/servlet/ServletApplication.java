0c86e3650634057e83e7d2d039d5b71a256a79b7
hyunhii <be_hyunny@naver.com>
2023-10-09 09:06:22 +0900

    feat: 5.스프링 MVC - 구조 이해 - 뷰 리졸버

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -12,4 +12,9 @@ public class ServletApplication {
 		SpringApplication.run(ServletApplication.class, args);
 	}
 
+	//스프링부트가 application.properties에 저장한 설정정보를 가져와 아래와 같이 자동으로 등록한다.
+	/*@Bean
+	ViewResolver internalResourveViewResolver() {
+		return new InternalResourceViewResolver("/WEB/INF/views/", "jsp");
+	}*/
 }
